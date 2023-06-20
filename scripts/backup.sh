ae2ed88706d16aa3fa9a7a8288ca399359b807f0
Fausto M. Lagos S <fausto@autonomia.digital>
2023-06-20 01:21:49 -0500

    Tested with gpg keys export - OK

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export DEBIAN_FRONTEND=noninteractive
+# export DEBIAN_FRONTEND=noninteractive
 
 existing_rsync=`which rsync || echo ""`
 if [ -z "existing_rsync" ]; then
@@ -11,13 +11,19 @@ if [ -z "existing_rsync" ]; then
     su -c "apt -y install rsync >> /dev/null"
 fi
 
-export GPGKEYS=~/.gnupg
 export SSHKEYS=~/.ssh
 export GITCONFIG=~/.gitconfig
 export DOCUMENTS=~/Documents
 export PICTURES=~/Pictures
 export ATUIN_HISTORY=~/.local/share/atuin
 export DEST=/media/$(whoami)/support/backup
+export GPGKEYS=/media/$(whoami)/support/backup/gpgkeys
 
-rsync -au $GPGKEYS $SSHKEYS $DOCUMENTS $PICTURES $GITCONFIG $ATUIN_HISTORY $DEST
+rsync -au $SSHKEYS $DOCUMENTS $PICTURES $GITCONFIG $ATUIN_HISTORY $DEST
+
+# Github gpg CAD keys
+gpg --pinentry-mode loopback --export-secret-keys --armor 07D0D1142E8CC84D > $GPGKEYS/gpg_private_github_cad.asc
+gpg --pinentry-mode loopback --export --armor 07D0D1142E8CC84D > $GPGKEYS/gpg_public_github_cad.asc
+
+
 
