0ff1a5f193e1607ca4af0205dca6e2f2cedaf00b
Fausto M. Lagos S <piratax007@protonmail.ch>
2023-09-06 22:47:21 +0200

    Added script to install all the LaTeX related utils

---
+++ scripts/xubuntu/LaTeX_installation.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+cat <<END
++----------------------------------------------------------------+
+  Will be installed the essential LaTeX packages. This will
+  take a few minutes.
++----------------------------------------------------------------+
+END
+
+sudo apt -y install texlive-base texlive-bibtex-extra texlive-binaries texlive-extra-utils texlive-font-utils texlive-fonts-extra texlive-fonts-extra-links texlive-fonts-recommended texlive-formats-extra texlive-lang-english texlive-lang-spanish texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-luatex texlive-metapost texlive-pictures texlive-plain-generic texlive-pstricks texlive-publishers texlive-science texlive-xetex python3-pygments latexmk latexdiff >> /dev/null
+
+cat <<END
++----------------------------------------------------------------+
+  The LaTeX packages has been installed successfully.
++----------------------------------------------------------------+
+END
