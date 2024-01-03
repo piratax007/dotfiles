9f484821f83e2502a51d9c796a0d5f4704e3ad96
Fausto M. Lagos S. <piratax007@protonmail.ch>
2024-01-03 22:35:42 +0100

    Added features for python on emacs

---
+++ .emacs.d/elpa/evil-mc-0.0.3/evil-mc-pkg.el
@@ -0,0 +1,16 @@
+(define-package "evil-mc" "0.0.3" "Multiple cursors for evil-mode"
+  '((emacs "24.3")
+    (evil "1.2.12")
+    (cl-lib "0.5"))
+  :commit "be2259b8cedd62011b25ddbcc1774bbbe9a66c61" :authors
+  '(("Gabriel Adomnicai" . "gabesoft@gmail.com"))
+  :maintainers
+  '(("Gabriel Adomnicai" . "gabesoft@gmail.com"))
+  :maintainer
+  '("Gabriel Adomnicai" . "gabesoft@gmail.com")
+  :keywords
+  '("evil" "editing" "multiple-cursors" "vim" "evil-multiple-cursors" "evil-mc" "evil-mc")
+  :url "https://github.com/gabesoft/evil-mc")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
