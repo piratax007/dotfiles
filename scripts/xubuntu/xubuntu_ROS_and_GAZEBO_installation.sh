c15315d307160bf8719407ecfa305bf5b525d38e
Fausto M. Lagos S <piratax007@protonmail.ch>
2026-09-01 20:15:37 +0200

    Added universe repository

--- scripts/xubuntu/xubuntu_ROS_and_GAZEBO_installation.sh
+++ scripts/xubuntu/xubuntu_ROS_and_GAZEBO_installation.sh
@@ -18,6 +18,8 @@ if [ -z "$existing_software_properties_common" ]; then
     sudo apt -y install software-properties-common >> /dev/null
 fi
 
+sudo add-app-repository universe
+
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F'"' '{print $4}')
 
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
