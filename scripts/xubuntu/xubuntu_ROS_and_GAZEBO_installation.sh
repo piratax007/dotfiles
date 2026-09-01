1a8cdcda4348f7ac63491e5589a5949ddc463934
Fausto M. Lagos S <piratax007@protonmail.ch>
2026-09-01 20:59:28 +0200

    Installing ros-dev-tools at the beginning of the script

--- scripts/xubuntu/xubuntu_ROS_and_GAZEBO_installation.sh
+++ scripts/xubuntu/xubuntu_ROS_and_GAZEBO_installation.sh
@@ -20,14 +20,13 @@ fi
 
 sudo add-apt-repository universe
 sudo apt update && sudo apt -y full-upgrade
+sudo apt -y install ros-dev-tools >> /dev/null
 
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F'"' '{print $4}')
 
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
 
 sudo dpkg -i /tmp/ros2-apt-source.deb >> /dev/null
-
-sudo apt -y install ros-dev-tools >> /dev/null
 
 sudo apt update >> /dev/null && sudo apt -y upgrade >> /dev/null
 
